759c9518e503558f2cbbb783776eac7480b93e2a
swamptea <swamp.tea.91@gmail.com>
2022-08-23 16:39:43 +0300

    calc realization

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,12 +1,15 @@
 package org.example;
 public class Calc {
     public static double payment(double creditSum, double percent, int month){
-        return 0;
+        double monthPercent = percent / 1200;
+        double res = creditSum * (monthPercent/(1-(Math.pow(monthPercent + 1, (-month)))));
+        double scale = Math.pow(10, 2);
+        return Math.round(res * scale) / scale;
     }
     public static double creditSum (double payment, int month){
-        return 0;
+        return payment * month;
     }
     public static double overPayment (double credit, double creditSum){
-        return 0;
+        return creditSum - credit;
     }
 }
